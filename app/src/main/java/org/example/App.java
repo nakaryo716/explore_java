9dc47a631bce27c7db4b5f49160daf13c77288bc
ryoNAKAYA <nakayaryo0716@gmail.com>
2025-07-20 12:59:37 +0900

    initial commit

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        var helloApp = new HelloWorld();
+        helloApp.sayHelloWorld();
+    }
+}
---
+++ app/src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class HelloWorld {
+    public void sayHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
+
