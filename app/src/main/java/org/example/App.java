bd95ec472e173731702f251387ebed2583720c53
ryoNAKAYA <nakayaryo0716@gmail.com>
2025-07-25 21:48:58 +0900

    add class(struct) chapter

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        Flow.ifStateMent();
+        var p1 = new Point(9.2, 11.8, 0.5);
+        var p2 = new Point(7.0, 9.2, -12.0);
+
+        var addPoint = p1.add(p2);
+
+        System.out.printf("x: %f\n", addPoint.x());
+        System.out.printf("x: %f\n", addPoint.y());
+        System.out.printf("x: %f\n", addPoint.z());
     }
 }
---
+++ app/src/main/java/org/example/Point.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Point {
+    private final double x;
+    private final double y;
+    private final double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double x() {
+        return this.x;
+    }
+
+    public double y() {
+        return this.y;
+    }
+
+    public double z() {
+        return this.z;
+    }
+
+    public Point add(Point other) {
+        return new Point(this.x + other.x, this.y + other.y, this.z + other.z);
+    }
+}
+
